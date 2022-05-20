ca9b99df728c6663dce19bfaa955f3eb2cc39119
sangbaek <aumsbk@naver.com>
2022-05-20 23:28:04 +0900

    다이나믹 프로그래밍

---
+++ 다이나믹 프로그래밍/이것이 코딩테스트다_p210_피보나치 함수 소스코드.py
@@ -0,0 +1,7 @@
+# 피보나치 함수(Fibonacci Function)를 재귀 함수로 표현
+def fibo(x):
+    if x == 1 or x == 2:  # 피보나치 수 1과 2는 항상 1이기 때문에 1를 return 해주기
+        return 1
+    return fibo(x - 1) + fibo(x - 2) # 피보나치 점화식 코드
+
+print(fibo(4)) # 4번째 피보나치 수를 구하기
