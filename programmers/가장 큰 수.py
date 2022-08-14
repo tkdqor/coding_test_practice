de58f7205b24304375a451009bc9a917dd5f854f
sangbaek <aumsbk@naver.com>
2022-08-14 16:14:38 +0900

    Add : 프로그래머스 문제 추가 - 정렬 문제 추가

---
+++ programmers/가장 큰 수.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    # 리스트 요소를 문자열로 바꾸고 리스트로 설정
+    input_list = list(map(str, numbers))
+    
+    # sort함수의 key 매개변수를 사용해서 정렬 기준 설정
+    # 문자열의 길이를 3배로 늘려서 비교
+    input_list.sort(key = lambda x:x*3, reverse=True)
+    answer = ''
+    answer = answer.join(input_list)
+    # 마지막으로 answer를 int로 바꾸고 str로 바꾸는 이유는 "0000"과 같은 케이스를 0으로 바꾸고 문자열로 출력하기 위함이다.
+    return str(int(answer))
+
+print(solution([3, 30, 34, 5, 9]))
---
+++ programmers/카펫.py
@@ -0,0 +1,16 @@
+def solution(brown, yellow):
+    # 카펫 가로 * 세로 = 갈색 격자 개수 + 노란색 격자 개수
+    nm = brown + yellow
+    # 가로를 의미하는 n를 구하는 과정 시작
+    # nm이 12라면, n이 1부터 12까지 나올 수 있다. 가로12 * 세로1도 가능
+    for n in range(1, nm+1):
+        # nm을 n으로 나눴을 때 나머지가 있다면 그 수는 가로가 될 수 없다.
+        if nm%n != 0:
+            continue
+        # nm을 n으로 나눴을 때 딱 떨어진다면 해당 몫을 m으로 지정 / 즉, 세로가 된다
+        m = nm//n
+        # 카펫의 가로에서 2를 뺀 값과 세로에서 2를 뺸 값을 곱하면 노란색 격자 개수가 되는지 확인
+        if (n-2)*(m-2) == yellow:
+            return sorted([n, m], reverse = True)
+
+print(solution(10, 2))
