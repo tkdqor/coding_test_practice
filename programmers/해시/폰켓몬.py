fa3a5775153e71e8b521e7c480bb13efe87678bb
sangbaek <aumsbk@naver.com>
2022-08-16 11:56:55 +0900

    Add : 프로그래머스 문제 코드 추가 - 해시 폰켓몬 문제 코드 추가

--- programmers/해시/폰켓몬.py
+++ programmers/해시/폰켓몬.py
@@ -6,3 +6,22 @@ def solution(nums):
         answer = c
     
     return answer
+
+
+# 해시로 풀어본 나의 풀이
+def solution(nums):
+    hash_map = {}
+    for num in nums:
+        # 해시 키에 없다면 새로 추가하는 것으로 중복 제거
+        if num not in hash_map:
+            hash_map[num] = 1       
+    
+    if len(nums)//2 >= len(hash_map):
+        result = len(hash_map)
+    else:
+        result = len(nums)//2
+    
+    return result
+
+
+# 리스트에 요소의 종류가 다양할 때, 중복을 제거하기 위해 해시를 사용할 생각을 할 수 있다.
